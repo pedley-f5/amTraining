e442766de454ef67f1911c225ed2c4f1147d065d
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2023-01-11 19:18:37 +1100

    Update uninstall_Aspenmesh_all.sh

--- exercises/ex1-am-install/4-am-all-components-uninstall/uninstall_Aspenmesh_all.sh
+++ exercises/ex1-am-install/4-am-all-components-uninstall/uninstall_Aspenmesh_all.sh
@@ -10,6 +10,15 @@ sleep 3
 helm -n istio-system uninstall istiod
 sleep 3
 helm -n istio-system uninstall istio-base
-sleep 4
+sleep 3
+kubectl -n istio-system delete crd --all
+sleep 3
+kubectl -n istio-system delete jobs --all
+sleep 3
 kubectl delete ns istio-system
+sleep 3
+kubectl get clusterroles | grep aspen-mesh | awk kubectl '{print $1}' |xargs -n1 kubectl delete clusterrole
+sleep 3
+kubectl get clusterroles | grep istio | awk kubectl '{print $1}' |xargs -n1 kubectl delete clusterrole
+sleep 3
 echo "*********Aspenmesh uninstalled - See you soon :) *********"
