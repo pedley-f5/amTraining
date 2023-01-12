221a9412525583642e9a7ffd8123bba81ac24b9d
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2023-01-12 22:16:13 +1100

    Update command_script.sh

--- exercises/ex4-ingress-Gateway/2-myapp-deployment/command_script.sh
+++ exercises/ex4-ingress-Gateway/2-myapp-deployment/command_script.sh
@@ -4,3 +4,4 @@ kubectl label ns myapp istio-injection=enabled
 kubectl get namespace -L istio-injection
 kubectl -n myapp create secret tls myapp-certs --key certs/server.key  --cert certs/server.crt
 kubectl -n myapp create configmap myapp-configmap --from-file=nginx.conf=./nginx.conf
+kubectl -n myapp create -f myapp_deployment.yaml
