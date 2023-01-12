267fb9781e736aed162c8b2c6a4c797397133007
root <root@ip-172-31-12-84.ap-south-1.compute.internal>
2023-01-12 11:13:08 +0000

    server certs added for myapp

--- exercises/ex4-ingress-Gateway/2-myapp-deployment/command_script.sh
+++ exercises/ex4-ingress-Gateway/2-myapp-deployment/command_script.sh
@@ -2,5 +2,5 @@
 kubectl create ns myapp
 kubectl label ns myapp istio-injection=enabled
 kubectl get namespace -L istio-injection
-kubectl -n myapp create secret tls myapp-certs --key server.key  --cert server.crt
+kubectl -n myapp create secret tls myapp-certs --key certs/server.key  --cert certs/server.crt
 kubectl -n myapp create configmap myapp-configmap --from-file=nginx.conf=./nginx.conf
