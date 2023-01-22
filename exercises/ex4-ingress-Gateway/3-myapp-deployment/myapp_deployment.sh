0ef8703ba686100b1754c103df693c828ddf46c7
pedley-f5 <j.pedley@f5.com>
2023-01-22 17:40:18 +0000

    Updated paths

--- exercises/ex4-ingress-Gateway/3-myapp-deployment/myapp_deployment.sh
+++ exercises/ex4-ingress-Gateway/3-myapp-deployment/myapp_deployment.sh
@@ -2,6 +2,6 @@
 kubectl create ns myapp
 kubectl label ns myapp istio-injection=enabled
 kubectl get namespace -L istio-injection
-kubectl -n myapp create secret tls myapp-certs --key exercises/ex4-ingress-Gateway/certs/server.key --cert exercises/ex4-ingress-Gateway/certs/server.crt
-kubectl -n myapp create configmap myapp-configmap --from-file=nginx.conf=exercises/ex4-ingress-Gateway/2-myapp-deployment/nginx.conf
-kubectl -n myapp create -f exercises/ex4-ingress-Gateway/2-myapp-deployment/myapp_deployment.yaml
+kubectl -n myapp create secret tls myapp-certs --key ../certs/server.key --cert ../certs/server.crt
+kubectl -n myapp create configmap myapp-configmap --from-file=nginx.conf
+kubectl -n myapp create -f myapp_deployment.yaml
